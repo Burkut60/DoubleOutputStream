55b9e003145ea04597b749ea2092f9294939a7cc
Burkut60 <=kamilla2016@111>
2021-01-31 13:42:45 +0300

    fix writeLine no the end

--- src/DoubleOutputStream1.java
+++ src/DoubleOutputStream1.java
@@ -7,24 +7,22 @@ public class DoubleOutputStream1 extends OutputStream {
 
     protected OutputStream output1;
     protected OutputStream output2;
+  
 
     public DoubleOutputStream1(OutputStream output1, OutputStream output2) {
+
         this.output1 = output1;
         this.output2 = output2;
     }
 
-    public void printWriter(String a) {
-
-        PrintWriter print = new PrintWriter(output1);
-        PrintWriter print2 = new PrintWriter(output2);
-        print.print(a);
-        print2.print(a);
-        print.close();
-        print2.close();
-    }
-
     @Override
     public void write(int b) throws IOException {
-
+this.output1.write(b);
+this.output2.write(b);
+    }
+    
+    public void close() throws IOException {
+       this.output1.close();
+       this.output2.close();
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -4,13 +4,14 @@ public class Test {
 
 
     public static void main(String[] args) throws IOException {
-        String a = "Good day";
+        int a =1111331;
 
         OutputStream outputStream11 = new FileOutputStream("C:\\Users\\123\\Downloads\\save.txt");
         OutputStream outputStream22 = new FileOutputStream("C:\\Users\\123\\Desktop\\Новый текстовый документ.txt");
-        DoubleOutputStream1 doubleOutputStream1 = new DoubleOutputStream1(outputStream11, outputStream22);
-        doubleOutputStream1.printWriter(a);
 
+DoubleOutputStream1 doubleOutputStream1 = new DoubleOutputStream1(outputStream11,outputStream22);
+doubleOutputStream1.write(a);
+doubleOutputStream1.close();
     }
 }
 
